4ce03fc6b51d483f8d0fac402ff36091bc55c1fb
Kamil Nowak <kamilnowakx98@gmail.com>
2021-11-21 12:40:01 +0100

    first commit

---
+++ src/main/java/pl/kamilnowak/flatrentalmanagementsystem/FlatrentalmanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package pl.kamilnowak.flatrentalmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatrentalmanagementsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlatrentalmanagementsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kamilnowak/flatrentalmanagementsystem/FlatrentalmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kamilnowak.flatrentalmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatrentalmanagementsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
