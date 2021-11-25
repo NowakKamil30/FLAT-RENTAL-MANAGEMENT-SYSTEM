f2729a76e8a1dc115f03e4316841b901fb921f66
Kamil Nowak <kamilnowakx98@gmail.com>
2021-11-25 01:00:25 +0100

    implement basic services

--- src/main/java/pl/kamilnowak/flatrentalmanagementsystem/apartmet/service/ApartmentService.java
+++ src/main/java/pl/kamilnowak/flatrentalmanagementsystem/apartmet/service/ApartmentService.java
@@ -1,2 +1,58 @@
-package pl.kamilnowak.flatrentalmanagementsystem.apartmet.service;public class ApartmentService {
+package pl.kamilnowak.flatrentalmanagementsystem.apartmet.service;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import pl.kamilnowak.flatrentalmanagementsystem.apartmet.entity.Apartment;
+import pl.kamilnowak.flatrentalmanagementsystem.apartmet.repository.ApartmentRepository;
+import pl.kamilnowak.flatrentalmanagementsystem.service.CRUDOperation;
+import pl.kamilnowak.flatrentalmanagementsystem.service.PageableHelper;
+
+@Service
+@Log4j2
+public class ApartmentService implements CRUDOperation<Apartment, Long> {
+
+    private final ApartmentRepository apartmentRepository;
+    private final PageableHelper pageableHelper;
+
+    @Autowired
+    public ApartmentService(ApartmentRepository apartmentRepository, PageableHelper pageableHelper) {
+        this.apartmentRepository = apartmentRepository;
+        this.pageableHelper = pageableHelper;
+    }
+
+    @Override
+    public Apartment createObject(Apartment apartment) {
+        log.debug("create apartment");
+        return apartmentRepository.save(apartment);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        log.debug("delete apartment id: " + aLong);
+        apartmentRepository.deleteById(aLong);
+    }
+
+    @Override
+    public Apartment getObjectById(Long aLong) {
+        log.debug("read apartment id: " + aLong);
+        return apartmentRepository.getById(aLong);
+    }
+
+    @Override
+    public Page<Apartment> getAllObject(int page) {
+        log.debug("read apartments page: " + page);
+        return apartmentRepository.findAll(pageableHelper.countPageable(page));
+    }
+
+    @Override
+    public Apartment updateObject(Apartment apartment, Long aLong) {
+        log.debug("update apartment id: " + aLong);
+        if(apartmentRepository.findById(aLong).isEmpty()) {
+            return apartmentRepository.save(apartment);
+        }
+        apartment.setId(aLong);
+        return apartmentRepository.save(apartment);
+    }
 }
--- src/main/java/pl/kamilnowak/flatrentalmanagementsystem/apartmet/service/TenantService.java
+++ src/main/java/pl/kamilnowak/flatrentalmanagementsystem/apartmet/service/TenantService.java
@@ -1,2 +1,58 @@
-package pl.kamilnowak.flatrentalmanagementsystem.apartmet.service;public class TenantService {
+package pl.kamilnowak.flatrentalmanagementsystem.apartmet.service;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import pl.kamilnowak.flatrentalmanagementsystem.apartmet.entity.Tenant;
+import pl.kamilnowak.flatrentalmanagementsystem.apartmet.repository.TenantRepository;
+import pl.kamilnowak.flatrentalmanagementsystem.service.CRUDOperation;
+import pl.kamilnowak.flatrentalmanagementsystem.service.PageableHelper;
+
+@Service
+@Log4j2
+public class TenantService implements CRUDOperation<Tenant, Long> {
+
+    private final TenantRepository tenantRepository;
+    private final PageableHelper pageableHelper;
+
+    @Autowired
+    public TenantService(TenantRepository tenantRepository, PageableHelper pageableHelper) {
+        this.tenantRepository = tenantRepository;
+        this.pageableHelper = pageableHelper;
+    }
+
+    @Override
+    public Tenant createObject(Tenant tenant) {
+        log.debug("create tenant");
+        return tenantRepository.save(tenant);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        log.debug("delete tenant id: " + aLong);
+        tenantRepository.deleteById(aLong);
+    }
+
+    @Override
+    public Tenant getObjectById(Long aLong) {
+        log.debug("read tenant id: " + aLong);
+        return tenantRepository.getById(aLong);
+    }
+
+    @Override
+    public Page<Tenant> getAllObject(int page) {
+        log.debug("read tenants page: " + page);
+        return tenantRepository.findAll(pageableHelper.countPageable(page));
+    }
+
+    @Override
+    public Tenant updateObject(Tenant tenant, Long aLong) {
+        log.debug("update tenant id: " + aLong);
+        if(tenantRepository.findById(aLong).isEmpty()) {
+            return tenantRepository.save(tenant);
+        }
+        tenant.setId(aLong);
+        return tenantRepository.save(tenant);
+    }
 }
