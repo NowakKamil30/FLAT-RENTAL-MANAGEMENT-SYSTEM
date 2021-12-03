b664fccf1100a41b033b7d239c91e13516d8aab8
Kamil Nowak <kamilnowakx98@gmail.com>
2021-12-04 00:40:00 +0100

    read property from aplication.properties

--- src/main/java/pl/kamilnowak/flatrentalmanagementsystem/util/ConfigInfo.java
+++ src/main/java/pl/kamilnowak/flatrentalmanagementsystem/util/ConfigInfo.java
@@ -1,2 +1,20 @@
-package pl.kamilnowak.flatrentalmanagementsystem.util;public class ConfigInfo {
+package pl.kamilnowak.flatrentalmanagementsystem.util;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+public class ConfigInfo {
+    @Value("${config.page.url}")
+    private String pageURL;
+    @Value("${config.page.verification.account}")
+    private String verificationAccount;
+    @Value("${config.page.change.password}")
+    private String changePassword;
+    @Value("${config.secret.key}")
+    private String secretKey;
 }
