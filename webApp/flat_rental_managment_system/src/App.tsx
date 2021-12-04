7a7b7ca0ae4f81e98c49cd6915332866c896d1c6
Kamil Nowak <kamilnowakx98@gmail.com>
2021-12-04 11:25:18 +0100

    add theme

--- webApp/flat_rental_managment_system/src/App.tsx
+++ webApp/flat_rental_managment_system/src/App.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { ThemeProvider } from '@mui/system';
+import { createMuiTheme } from '@mui/material';
+import { blue, teal } from '@mui/material/colors';
+
+const theme = createMuiTheme({
+  palette: {
+      background : {
+        paper: teal[300]
+      },
+      primary: {
+          main: teal[500],
+          contrastText: '#000000'
+      },
+      secondary: {
+          main: blue[500],
+          contrastText: '#000000'
+      }
+  }
+});
 
 function App() {
+  const store = createStore(null, applyMiddleware(thunk));
+
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <header className="App-header">
+          </header>
+        </div>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
